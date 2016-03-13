b19329953782db352c231f9fe11856cc1a07f5ad
Marcel Rieger <marcelrieger@me.com>
2016-03-13 22:50:14 +0100

    Minor update in dtype_map script.

--- scripts/dtype_map.py
+++ scripts/dtype_map.py
@@ -26,7 +26,7 @@ for attr in dir(types_pb2):
     if attr.startswith("DT_"):
         tf_type_enum = getattr(types_pb2, attr)
         try:
-            dtype_map[tf_type_enum] = "np." + tf.DType(tf_type_enum).as_numpy_dtype.__name__
+            dtype_map[tf_type_enum] = "np." + tf.as_dtype(tf_type_enum).as_numpy_dtype.__name__
         except:
             pass
 

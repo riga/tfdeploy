f607c554911cc39795c3ca696cdb0a747e291e04
Marcel Rieger <marcelrieger@me.com>
2016-12-23 11:44:37 +0100

    Bump version to 0.3.3.

--- tfdeploy.py
+++ tfdeploy.py
@@ -12,7 +12,7 @@ __credits__    = ["Marcel Rieger"]
 __contact__    = "https://github.com/riga/tfdeploy"
 __license__    = "MIT"
 __status__     = "Development"
-__version__    = "0.3.2"
+__version__    = "0.3.3"
 
 __all__ = ["Model", "Tensor", "Operation", "Ensemble",
            "UnknownOperationException", "OperationMismatchException",
